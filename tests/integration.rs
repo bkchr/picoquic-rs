adf1d654e6329b063ac7b33cb949e241bfc4a348
Bastian Köcher <git@kchr.de>
2018-01-30 13:13:39 +0100

    Check that the server receives the data

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,7 +3,7 @@ extern crate futures;
 extern crate picoquic;
 extern crate tokio_core;
 
-use picoquic::{Config, Context, SMessage};
+use picoquic::{CMessage, Config, Context, SMessage};
 
 use std::net::SocketAddr;
 use std::thread;
@@ -13,7 +13,7 @@ use std::fmt;
 use futures::{Future, Sink, Stream as FStream};
 use futures::sync::mpsc::unbounded;
 
-use tokio_core::reactor::Core;
+use tokio_core::reactor::{Core, Handle};
 
 use bytes::Bytes;
 
@@ -41,7 +41,7 @@ fn create_context_and_evt_loop() -> (Context, Core) {
 
 fn start_server_thread<F, R>(create_future: F) -> SocketAddr
 where
-    F: 'static + Send + FnOnce(Context) -> R,
+    F: 'static + Send + FnOnce(Context, Handle) -> R,
     R: Future,
     <R as Future>::Error: fmt::Debug,
 {
@@ -53,8 +53,9 @@ where
         send.send(context.local_addr())
             .expect("sends server socket addr");
 
+        let handle = evt_loop.handle();
         evt_loop
-            .run(create_future(context))
+            .run(create_future(context, handle))
             .expect("event loop spins on server context");
     });
 
@@ -63,17 +64,36 @@ where
 
 #[test]
 fn server_start() {
-    start_server_thread(|c| c.for_each(|_| Ok(())));
+    start_server_thread(|c, _| c.for_each(|_| Ok(())));
 }
 
 #[test]
 fn client_connects_creates_stream_and_sends_data() {
+    let send_data = "hello server";
     let (send, recv) = unbounded();
 
-    let addr = start_server_thread(move |c| {
-        c.for_each(move |_| {
-            let _ = send.clone().unbounded_send(true);
-            Ok(())
+    let addr = start_server_thread(move |c, h| {
+        c.for_each(move |c| {
+            let h = h.clone();
+            let send = send.clone();
+            c.for_each(move |s| {
+                let h = h.clone();
+                let send = send.clone();
+                match s {
+                    CMessage::NewStream(s) => h.clone().spawn(s.for_each(move |m| {
+                        match m {
+                            SMessage::Data(data) => {
+                                let _ = send.clone()
+                                    .unbounded_send(String::from_utf8(data.to_vec()).unwrap());
+                            }
+                            _ => {}
+                        };
+                        Ok(())
+                    }).map_err(|_| ())),
+                    _ => {}
+                };
+                Ok(())
+            })
         })
     });
 
@@ -87,9 +107,13 @@ fn client_connects_creates_stream_and_sends_data() {
         .run(con.new_bidirectional_stream())
         .expect("creates stream");
 
-    evt_loop
-        .run(stream.send(SMessage::Data(Bytes::from("hello server"))))
+    // The stream must not be dropped here!
+    let _stream = evt_loop
+        .run(stream.send(SMessage::Data(Bytes::from(send_data))))
         .unwrap();
 
-    assert!(evt_loop.run(recv.into_future()).unwrap().0.unwrap());
+    assert_eq!(
+        send_data,
+        evt_loop.run(recv.into_future()).unwrap().0.unwrap()
+    );
 }
