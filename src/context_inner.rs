86f5d870d2c0cf576f0d6ca011edfbfd23ca72bc
Bastian Köcher <git@kchr.de>
2018-12-28 22:08:24 +0100

    Send all available packets of a connection

--- src/context_inner.rs
+++ src/context_inner.rs
@@ -158,17 +158,20 @@ impl ContextInner {
                 con.delete();
                 break;
             } else {
-                match con.prepare_packet(&mut self.buffer, current_time) {
-                    Ok(Some((len, addr))) => {
-                        if !self.send_data_in_buffer(len, addr) {
-                            return;
+                loop {
+                    match con.prepare_packet(&mut self.buffer, current_time) {
+                        Ok(Some((len, addr))) => {
+                            if !self.send_data_in_buffer(len, addr) {
+                                return;
+                            }
                         }
-                    }
-                    Ok(None) => {}
-                    Err(e) => {
-                        debug!("error while sending connections packets: {:?}", e);
-                    }
-                };
+                        Ok(None) => break,
+                        Err(e) => {
+                            debug!("error while sending connections packets: {:?}", e);
+                            break;
+                        }
+                    };
+                }
             }
         }
     }
